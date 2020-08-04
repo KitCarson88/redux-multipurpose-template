f20d3641f9df3aeea3fcf3133aedf11214bbca97
Simone Colazzo <colsimo88@gmail.com>
2020-08-04 18:28:25 +0200

    Updated versions

--- src/store/epics.ts
+++ src/store/epics.ts
@@ -1,7 +1,10 @@
 import { combineEpics } from 'redux-observable-es6-compat';
 
+//Epics imports: PLEASE DON'T DELETE THIS PLACEHOLDER
+
 export default function rootEpic()
 {
     return combineEpics(
+        //Epics: PLEASE DON'T DELETE THIS PLACEHOLDER
     );
 }
