102aa66e1a1028235bf58a37e152c519c18dc41a
Simone Colazzo <colsimo88@gmail.com>
2020-08-17 10:31:17 +0200

    Updated multipurpose cli

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,10 +1,5 @@
 import { select, call, delay, put, takeEvery, takeLatest, take, all,  } from 'redux-saga/effects';
 
-function sagaRegistrationLog(name, actionType)
-{
-    console.log("Registering saga " + name + " on " + actionType);
-}
-
 export default function* rootSaga()
 {
     yield all([
